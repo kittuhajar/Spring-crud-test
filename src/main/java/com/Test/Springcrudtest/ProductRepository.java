2713ef287f184d9165d0a545ba4aacd4d1ceef20
krishnakanth hajar <krishna.h@etggs.com>
2020-07-07 13:05:09 +0530

    commenting

--- src/main/java/com/Test/Springcrudtest/ProductRepository.java
+++ src/main/java/com/Test/Springcrudtest/ProductRepository.java
@@ -2,6 +2,7 @@ package com.Test.Springcrudtest;
 
 import org.springframework.data.repository.CrudRepository;
 
+//crud Repository
 public interface ProductRepository extends CrudRepository<Product, Integer>{
 	public Product findByName(String name);
 
